f8a44e1f6cb5e1dee93e0ac9dcf23f35f60e11ff
Dale B <daleban.dev@gmail.com>
2023-10-16 12:19:55 +0800

    chore: create index import for images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,25 @@
+import devfestLogo from "./devfest-logo.png";
+import cloud from "./cloud.svg";
+import gdgLogo from "./gdg-logo.png";
+import gdgWtmLogo from "./gdg-wtm-logo.png";
+import globeIcon from "./globe.svg";
+import kiteIcon from "./kite.svg";
+import masskaraSet from "./masskara-set.png";
+import masskara from "./masskara.png";
+import peopleIcon from "./people.svg";
+import waveIcon from "./wave.svg";
+import whitePaperTexture from "./white-paper-texture.png";
+
+export {
+  devfestLogo,
+  cloud,
+  gdgLogo,
+  gdgWtmLogo,
+  globeIcon,
+  kiteIcon,
+  masskaraSet,
+  masskara,
+  peopleIcon,
+  waveIcon,
+  whitePaperTexture,
+};
