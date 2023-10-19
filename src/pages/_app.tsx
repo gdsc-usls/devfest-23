f83edb223e70b5d246d5cc386ef7192a2b60a2e2
Josh Daniel <joshxfi.dev@gmail.com>
2023-10-19 15:55:51 +0800

    feat: add google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "sonner";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import Router from "next/router";
+import localFont from "next/font/local";
 import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
@@ -14,9 +15,16 @@ Router.events.on("routeChangeStart", () => {
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
+const googleReg = localFont({
+  src: "./Google-Sans-Regular.woff2",
+  display: "swap",
+  weight: "400",
+  variable: "--font-google-reg",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <Layout className={googleReg.className}>
       <Toaster />
       <Component {...pageProps} />
     </Layout>
